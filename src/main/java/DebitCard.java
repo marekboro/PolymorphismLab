3990da8b06c2fb4a1bf7d1115aa6832425e5d5b1
marekboro <marek.borowicz83@gmail.com>
2020-09-28 14:47:55 +0100

    refactored to abstract Card out of debitCard and CreditCard

--- src/main/java/DebitCard.java
+++ src/main/java/DebitCard.java
@@ -1,38 +1,20 @@
-public class DebitCard implements IScan{
-    private String cardNumber;
+public class DebitCard extends Card implements IScan{
     private int sortCode;
     private int accountNumber;
-    private String expiryDate;
-    private int securityNumber;
 
     public DebitCard(String cardNumber, int sortCode, int accountNumber, String expiryDate, int securityNumber) {
-        this.cardNumber = cardNumber;
+        super(cardNumber,expiryDate,securityNumber);
         this.sortCode = sortCode;
         this.accountNumber = accountNumber;
-        this.expiryDate = expiryDate;
-        this.securityNumber = securityNumber;
-    }
 
-    public String getCardNumber() {
-        return this.cardNumber;
     }
 
     public int getSortCode() {
         return this.sortCode;
     }
-
     public int getAccountNumber() {
         return this.accountNumber;
     }
-
-    public String getExpiryDate() {
-        return this.expiryDate;
-    }
-
-    public int getSecurityNumber() {
-        return this.securityNumber;
-    }
-
     public String scan() {
         return "Payment Complete";
     }
